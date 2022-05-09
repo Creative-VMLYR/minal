040886de479bb63ea5d04b9d193c81f8d91704a0
briangatarwacreative <briangatarwacreative@gmail.com>
2022-05-09 17:12:42 +0300

    appointment.js errors cleanup

--- pages/appointment.js
+++ pages/appointment.js
@@ -1,21 +1,21 @@
 import React from "react";
 
 // API
-import { getAppointmentFormData } from "../lib/api";
+import { getAppointmentFormData, getPrimaryMenuItems } from "../lib/api";
 
 // Components
 import Form1 from "../components/forms/Form1";
-import Navigation2 from "../components/navigation/Navigation2";
+import Navigation from "../components/navigation/Navigation";
 
 // Components
 import PageHead from "../components/pageHead/PageHead";
 
-export default function Appointment({ formData }) {
+export default function Appointment({ formData, menuItemsData }) {
   return (
     <>
       <main className="page-wrap position-relative bg-teal-650">
         <PageHead />
-        <Navigation2 posAbs={false} />
+        <Navigation menuItemsData={menuItemsData} />
         <Form1 formData={formData} />
       </main>
     </>
@@ -24,10 +24,12 @@ export default function Appointment({ formData }) {
 
 export async function getStaticProps() {
   const formDataRes = await getAppointmentFormData();
+  const menuItemsDataRes = await getPrimaryMenuItems();
 
   return {
     props: {
       formData: formDataRes ?? null,
+      menuItemsData: menuItemsDataRes ?? null,
     },
   };
 }
